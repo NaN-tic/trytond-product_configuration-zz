4350c9e2abf9e23d713cb5432deb9fa5699c21b1
resteve <resteve@zikzakmedia.com>
2014-02-20 18:27:59 +0100

    Revert product configuration simple class

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 #This file is part product_configuration module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
+
 from trytond.pool import Pool
 from .configuration import *
 
-
 def register():
     Pool.register(
-        Configuration,
+        ProductConfiguration,
+        ProductConfigurationCompany,
         module='product_configuration', type_='model')
--- configuration.py
+++ configuration.py
@@ -5,10 +5,69 @@ from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 from trytond.pool import Pool
 from trytond.transaction import Transaction
 
-__all__ = ['Configuration']
+__all__ = [
+    'ProductConfiguration',
+    'ProductConfigurationCompany',
+]
 
 
-class Configuration(ModelSingleton, ModelSQL, ModelView):
+class ProductConfiguration(ModelSingleton, ModelSQL, ModelView):
     'Product Configuration'
     __name__ = 'product.configuration'
 
+    @classmethod
+    def get_fields(cls, configurations, names):
+        res = {}
+        ConfigurationCompany = Pool().get('product.configuration.company')
+        company_id = Transaction().context.get('company')
+        conf_id = configurations[0].id
+        if company_id:
+            confs = ConfigurationCompany.search([
+                ('company', '=', company_id),
+                ], limit=1)
+            if confs:
+                for conf in confs:
+                    for field_name in names:
+                        value = getattr(conf, field_name)
+                        if value and (not isinstance(value, unicode) 
+                            and not isinstance(value, int)):
+                                value = value.id
+                        res[field_name] = {conf_id: value}
+            else:
+                for field_name in names:
+                    res[field_name] = {conf_id: None}
+        return res
+
+    @classmethod
+    def set_fields(cls, configurations, name, value):
+        if value:
+            ConfigurationCompany = Pool().get('product.configuration.company')
+            company_id = Transaction().context.get('company')
+            if company_id:
+                configuration = ConfigurationCompany.search([
+                    ('company', '=', company_id),
+                    ], limit=1)
+                if not configuration:
+                    vlist = [{
+                            'company': company_id,
+                            name: value,
+                            }]
+                    ConfigurationCompany.create(vlist)
+                else:
+                    ConfigurationCompany.write([configuration[0]],
+                                                 {name: value})
+
+
+class ProductConfigurationCompany(ModelSQL, ModelView):
+    'Product Configuration Company'
+    __name__ = 'product.configuration.company'
+    company = fields.Many2One('company.company', 'Company',
+        required=True, readonly=True)
+
+    @classmethod
+    def __setup__(cls):
+        super(ProductConfigurationCompany, cls).__setup__()
+        cls._sql_constraints += [
+            ('company_uniq', 'UNIQUE(company)',
+                'There is already one configuration for this company.'),
+        ]
